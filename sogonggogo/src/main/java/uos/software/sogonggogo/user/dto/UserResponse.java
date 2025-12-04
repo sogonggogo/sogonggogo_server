d503d42572b69a3f77b7d13be6aebdcde0739d98
ddoong10 <ddoong10@naver.com>
2025-12-04 13:06:29 +0900

    사용자 인증/인가 구현 완료 + response 형태 수정

--- sogonggogo/src/main/java/uos/software/sogonggogo/user/dto/UserResponse.java
+++ sogonggogo/src/main/java/uos/software/sogonggogo/user/dto/UserResponse.java
@@ -8,24 +8,16 @@ public record UserResponse(
 		String address,
 		String phoneNumber,
 		String email,
-		String creditCardMasked
+		String creditCardNumber
 ) {
 
 	public static UserResponse from(User user) {
 		return new UserResponse(
-				user.getId(),
-				user.getName(),
-				user.getAddress(),
-				user.getPhoneNumber(),
-				user.getEmail(),
-				maskCreditCard(user.getCreditCardNumber()));
-	}
-
-	private static String maskCreditCard(String creditCardNumber) {
-		if (creditCardNumber == null || creditCardNumber.length() < 4) {
-			return null;
-		}
-		String last4 = creditCardNumber.substring(creditCardNumber.length() - 4);
-		return "************" + last4;
+			user.getId(),
+			user.getName(),
+			user.getAddress(),
+			user.getPhoneNumber(),
+			user.getEmail(),
+			user.getCreditCardNumber());
 	}
 }
