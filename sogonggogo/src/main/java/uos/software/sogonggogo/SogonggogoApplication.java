af12ed63b597d03583a4eb25906f7592d9ae82c8
ddoong10 <ddoong10@naver.com>
2025-12-04 11:21:12 +0900

    spring init

---
+++ sogonggogo/src/main/java/uos/software/sogonggogo/SogonggogoApplication.java
@@ -0,0 +1,13 @@
+package uos.software.sogonggogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SogonggogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SogonggogoApplication.class, args);
+	}
+
+}
---
+++ sogonggogo/src/test/java/uos/software/sogonggogo/SogonggogoApplicationTests.java
@@ -0,0 +1,13 @@
+package uos.software.sogonggogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SogonggogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
